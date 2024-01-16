811f005300f867611561b6a1868c478312b13aaa
unknown <evandanieltedjadinata@gmail.com>
2024-01-16 21:03:26 +0700

    now

---
+++ js/Kalkulator.js
@@ -0,0 +1,27 @@
+var calcDisplay = ""
+function btnClick(e){
+    if (e == "1" || e == "2" || e == "3" || e == "4" || e == "/" || e == "*")
+        if (e == "AC"){
+            calcDisplay = ""
+        } else {
+            calcDisplay = calcDisplay + e
+            console.log(calcDisplay)
+        }
+        document.getElementById("display").value = calcDisplay
+    } else {
+        console.log("input harus berupa angka dan simbol")
+    }
+
+document.getElementById("display").addEventListener('input', function displayChange() {
+    console.log(this.value)
+    let e = this.value
+    
+let chars = /^\d[0-9+-*/.]+$/
+
+    if (chars.test(e)) {
+        console.log("digits")
+    } else {
+        console.log("not digits")
+    }
+})
+
